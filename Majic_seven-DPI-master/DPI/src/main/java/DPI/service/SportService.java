5afdcee7632af60d77277dcd4cac67d07d458839
zkj <123@qq.com>
2021-01-10 14:05:46 +0800

    删除了一些没用的文件

--- Majic_seven-DPI-master/DPI/src/main/java/DPI/service/SportService.java
+++
@@ -1,9 +0,0 @@
-package DPI.service;
-
-import DPI.entity.Sport;
-
-public interface SportService {
-	int updateByOpenid(Sport record);
-	Sport selectByOpenid(String openid,String exerciseDate);
-	int insertSport(Sport record);
-}
