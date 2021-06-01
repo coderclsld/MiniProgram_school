29852e23fff281d3cc61329609aabde464c33ae8
clsld <807686672@qq.com>
2021-06-01 20:16:02 +0800

    大致修改完成

--- app.js
+++ app.js
@@ -85,28 +85,25 @@ App({
           that.globalData.hasUserInfo = false
         }
       })
-      // wx.getSetting({
-      //   success: function (res) {
-      //     if (res.authSetting["scope.userInfo"]) {
-      //       // 已经授权，可以直接调用 getUserInfo 获取头像昵称
-      //       wx.getUserInfo({
-      //         success: function (res) {
-      //           that.globalData.userInfo = res.userInfo;
-      //           console.log(res.userInfo);
-      //           resolve(that.globalData.userInfo);
-      //         },
-      //         fail(err) {
-      //           console.log(err);
-      //           reject(err);
-      //         },
-      //       });
-      //     }
-      //   },
-      //   fail(err) {
-      //     reject(err);
-      //   },
-      // });
-
+      wx.getSetting({
+        success: function (res) {
+          if (res.authSetting["scope.userInfo"]) {
+            // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+            wx.getUserInfo({
+              success: function (res) {
+                resolve(that.globalData.userInfo);
+              },
+              fail(err) {
+                console.log(err);
+                reject(err);
+              },
+            });
+          }
+        },
+        fail(err) {
+          reject(err);
+        },
+      });
     });
   },
   // 连接websocket服务器
